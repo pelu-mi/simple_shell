9e5d9642977e14868f2359d4cf69ab84f0b42153
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-08-10 18:38:09 -0700

    Added necessary files

--- _builtin.c
+++ _builtin.c
@@ -5,22 +5,25 @@
  * @cmd: command to be entered
  * Return: function pointer of the builtin command
  */
+
 int (*get_builtin(char *cmd))(data_shell *)
 {
-	buitin_t builtin[] = {
+	builtin_t builtin[] = {
 		{ "env", _env },
 		{ "exit", exit_shell },
 		{ "setenv", _setenv },
-		{ "unsetenv", _unsetenv },					       { "cd", cd_shell }						      { "help", get_help },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
 		{ NULL, NULL }
-		};
-		int i;
+	};
+	int i;
 
-		for (i = 0; builtin[i].name; i++)
-		{
-			if (_strcmp(builtin[i].name, cmd) == 0)
-				break;
-		}
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
 
-		return (builtin[i].f);
-	}	
+	return (builtin[i].f);
+}
---
+++ exec_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * exec_line - finds builtins and commands
+ *
+ * @datash: data relevant (args)
+ * Return: cmd to be executed and 1 if null.
+ */
+
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
